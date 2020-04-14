f59ba90b3bfc8985c905ec7bffba954f7ea22142
Kevin McGinley <kevin.mcginley@orreco.com>
2020-04-14 10:40:32 +0100

    cookie and privacy policy updates

--- app/src/app/views/fevermap-landing-cookie-policy.js
+++ app/src/app/views/fevermap-landing-cookie-policy.js
@@ -28,17 +28,33 @@ class FevermapLandingCookiePolicy extends LitElement {
     <h1>Cookie Policy for My Covid Symptoms</h1>
     This is the Cookie Policy for My Covid Symptoms, accessible from mycovidsymptoms.org, mycovidsymptoms.com and mycovidsymptoms.ie.
     <h3>What Are Cookies</h3>
-    A cookie is a small piece of data that a website stores on your device when you visit, typically containing information about the website itself, a unique identifier that allows the site to recognise your web browser when you return, additional data that serves the purpose of the cookie, and the lifespan of the cookie itself.
+    A cookie is a small piece of data that a website stores on your device when you visit,
+    typically containing information about the website itself, a unique identifier that allows the
+    site to recognise your web browser when you return, additional data that serves the purpose
+    of the cookie, and the lifespan of the cookie itself.
     <h3>How We Use Cookies</h3>
-    We use a single cookie, which is the minimum amount of cookies necessary for MyCovidSymptoms to function. While you may disable cookies, it is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.
+    We use a single cookie, which is the minimum amount of cookies necessary for
+    MyCovidSymptoms to function. While you may disable cookies, it is recommended that you
+    leave on all cookies if you are not sure whether you need them or not in case they are used to
+    provide a service that you use. Our single cookie can be deleted by you at any time under
+    your choice and control.
     <h3>Disabling Cookies</h3>
-    You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.
+    You can prevent the setting of cookies by adjusting the settings on your browser (see your
+    browser Help for how to do this). Be aware that disabling cookies will affect the functionality
+    of this and many other websites that you visit. Disabling cookies will usually result in also
+    disabling certain functionality and features of the this site. Therefore it is recommended that
+    you do not disable cookies.
     <h3>The Cookies We Set</h3>
-    Surveys related cookies: We user cookies to store some of your response information so that you do not have to repeatedly enter details that never or infrequently change. Namely, we store your response to the year of birth, gender and location questions. Additionally, we store an anonymous unique identifier for your device so that we know which symptoms come from the same person.
+    Surveys related cookies: We user cookies to store some of your response information so that
+    you do not have to repeatedly enter details that never or infrequently change. Namely, we
+    store your response to the year of birth, gender and location questions. Additionally, we store
+    an anonymised date stamp unique identifier for your entry device so that we know which
+    symptoms come from the same person. The project team will conduct a deletion of their copy of this
+    identifier within 90 days of the data collection completing.
     <h3>Third Party Cookies</h3>
     We do not use any third party cookies
     <h3>Further Information</h3>
-    If you more information you can contact us via email at: mycovidsymptoms@gmail.com
+    If you want more information you can contact us via email at: mycovidsymptoms@gmail.com
     `
   }
 
--- app/src/app/views/fevermap-landing-privacy-policy.js
+++ app/src/app/views/fevermap-landing-privacy-policy.js
@@ -26,10 +26,33 @@ class FevermapLandingPrivacyPolicy extends LitElement {
   renderPolicy() {
     return html`
     <h1>Privacy Policy</h1>
-    <p>MyCovidSymptoms only collects and stores the anonymous answers you provide in the symptom survey, along with information on the time and date of the submissions.</p>
-    <p>An anonymous identifier is created from time of your first submission. This anonymous identifier allows us to understand which anonymous responses are from the same individual. From that we can understand how symptoms change over time.</p>
-    <p>MyCovidSymptoms does not collect any additional information. None of the data we collect is of a personal nature, nor could it be used to identify a user offline.</p>
-    <p>If you have any further questions about how MyCovidSymptoms handles the data you submit, please contact us at: mycovidsymptoms@gmail.com</p>
+    <p>The National University of Ireland Galway with University of Limerick and Orreco are
+    conducting this project.</p>
+    <p>MyCovidSymptoms only collects through your consent and stores the anonymioused answers
+    you provide in the symptom survey, along with information on the time and date of the
+    submissions. The aggregate anonymous data will be provided to the Irish health authorities to
+    help with their planning and will be used in research by NUI Galway to help prevent and
+    mitigate future pandemics.</p>
+    <p>An anonymised ous identifier is created from time of your first submission. This anonymised
+    ous identifier allows us to understand which anonymioused responses are from the same
+    individual. From that we can understand how symptoms change over time. As per the cookie
+    policy available on this website, this can be deleted by you at any time under your control.</p>
+    <p>The project team will conduct a deletion of their copy of this
+    identifier within 90 days of the data collection completing.</p>
+    <p>MyCovidSymptoms does not collect any additional information. None of the data we collect
+    is of a personal nature, nor will could it be used to identify a user offline.</p>
+    <p>If you have any further questions about how MyCovidSymptoms handles the data you
+    submit, please contact us at: mycovidsymptoms@gmail.com</p>
+    <h2>Data Protection<h2>
+    <p>If you have any data protection rights queries or wish to exercise your data protection rights
+    please see: <a href="https://www.nuigalway.ie/data-protection">https://www.nuigalway.ie/data-protection</a> or please contact:
+    dataprotection@nuigalway.ie</p>
+    <p>The Office of the Data Protection Commissioner can be contacted at:
+    <a href ="https://www.dataprotection.ie/en/contact/how-contact-us">https://www.dataprotection.ie/en/contact/how-contact-us</a></p>
+    <h2>Data Processor</h2>
+    <p>The project data is stored by Digital Ocean who are certified under the Privacy Shield
+    Principles. Please see this link for details on their data protection information:
+    <a href="https://www.digitalocean.com/legal">https://www.digitalocean.com/legal</a></p>
     `
   }
 
